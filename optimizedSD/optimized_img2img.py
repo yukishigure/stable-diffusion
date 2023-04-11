b048b073959423d47a6177453abff8396dd17cd8
Mimaki SiON <sion.git@pn.dojin.com>
2023-04-11 19:08:49 +0900

    unsupport plms for img2img

--- optimizedSD/optimized_img2img.py
+++ optimizedSD/optimized_img2img.py
@@ -175,7 +175,7 @@ parser.add_argument(
     "--sampler",
     type=str,
     help="sampler",
-    choices=["ddim", "plms", "heun", "euler", "euler_a", "dpm2", "dpm2_a", "lms"],
+    choices=["ddim", "heun", "euler", "euler_a", "dpm2", "dpm2_a", "lms"],
     default="ddim",
 )
 parser.add_argument(
